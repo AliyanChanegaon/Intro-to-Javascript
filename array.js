b51cb4a6137879078afcba4c0d9f5dd848059aa6
Aliyan Chanegaon <111250756+AliyanChanegaon@users.noreply.github.com>
2023-03-15 09:26:35 +0000

    Arrays Push and Pop

---
+++ array.js
@@ -0,0 +1,27 @@
+let name1="Sufi  ";
+let name2="Sufiy  ";
+let name3="Sufiya  ";
+let name4="Sufiyan  ";
+
+let name=[name1 , name2 , name3 , name4];
+console.log(name);
+
+// console.log(name.length)
+
+// console.log(stringify(name))
+
+//////////////////
+
+
+//push and pop works like stack last in first out.
+let no=[1,2,3,4,5,6];
+
+console.log(no.push(6));
+// WIll add 6 to last index
+console.log(no);
+
+no.pop();
+// To remove last element
+
+
+console.log(no);
