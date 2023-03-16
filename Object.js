6b53922f8190fb21a85906c494b75bb5f9c06846
Aliyan Chanegaon <111250756+AliyanChanegaon@users.noreply.github.com>
2023-03-16 12:53:38 +0000

    Objects in javascript.

---
+++ Object.js
@@ -0,0 +1,28 @@
+
+// Objects are called as key value pairs
+
+var student = {
+
+  name: "Aliyan", //name is a key and Aliyan is an value.
+  age: 22,
+  gender:"Male",
+  location:"Nashik"
+  
+};
+
+
+// ways to access value at a particular key
+// 1st. Bracket notation
+
+
+console.log(student["name"]);
+
+console.log(student["age"]);
+
+console.log(student["gender"]);
+
+// 2nd. Dot notation
+
+console.log("Dot notation")
+console.log(student.name);
+
