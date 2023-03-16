4c70fb70bdec1cc3e804cbef3586fed15d428480
Aliyan Chanegaon <111250756+AliyanChanegaon@users.noreply.github.com>
2023-03-16 06:33:39 +0000

    inbuilt function to convert numbers to string and vica versa.

---
+++ Inbuilt_F.js
@@ -0,0 +1,26 @@
+// To convert string to number;
+
+let a = "12";
+let b = "15";
+
+console.log(a,b);
+
+let x=Number(a);
+let y=Number(b);
+
+console.log(typeof(x),typeof(y));
+
+
+// To convert number to string;
+
+
+
+let c = 12;
+let d = 15;
+
+console.log(a,b);
+
+let u=toString(a);
+let v=toString(b);
+
+console.log(typeof(u),typeof(v));
